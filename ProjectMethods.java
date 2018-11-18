6351669a3759ab9965fdf8e99670a3982589dc15
TestLeafPages <koushik.chatterjee@testleaf.com>
2018-11-18 13:27:36 +0530

    Add files via upload

--- ProjectMethods.java
+++ ProjectMethods.java
@@ -1,17 +1,25 @@
 package wdmethods;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 
 public class ProjectMethods extends SeMethods{
+	
+	@DataProvider(name = "fetchData") 
+	public String[][] getData() throws IOException {
+		return ReadExcel.getExcelData(excelFileName);
+	}
 	@Parameters({"browser", "url"})
 	@BeforeMethod(groups = "config")
 	public void login(String bro, String url) {
 		startApp(bro, url);
 		WebElement eleUsername = locateElement("id", "username");
-		type(eleUsername, "DemoSalesManager");
+		type(eleUsername, "DemoCSR");
 		WebElement elePassword = locateElement("id", "password");
 		type(elePassword, "crmsfa");
 		WebElement eleLogin = locateElement("class", "decorativeSubmit");
@@ -23,5 +31,6 @@ public class ProjectMethods extends SeMethods{
 	}
 
 }
+
 
 
